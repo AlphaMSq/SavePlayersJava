4b09bd4d00156b5b9bb41e6c0be48877b67764e5
AlphaMSq <alphabaqpla@outlook.com>
2023-10-02 18:59:18 +0300

    upd

--- src/main/java/io/greitan/uniworlds/SavePlayers.java
+++ src/main/java/io/greitan/uniworlds/SavePlayers.java
@@ -1,16 +1,22 @@
 package io.greitan.uniworlds;
 
 import lombok.Getter; 
-import org.bukkit.plugin.java.JavaPlugin;
-import java.util.logging.Logger;
-import io.greitan.uniworlds.listeners.MainListener;
+
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import io.greitan.uniworlds.listeners.MainListener;
+import io.greitan.uniworlds.commands.TestCommand;
+
+import java.util.logging.Logger;
 
 public class SavePlayers extends JavaPlugin {
     @Getter
     private static SavePlayers instance;
+
     @Getter
     private static Logger jlogger;
+
     @Override
     public void onEnable() {
         instance = this; 
@@ -19,5 +25,7 @@ public class SavePlayers extends JavaPlugin {
       	jlogger.info("Plugin loaded!");
 
         Bukkit.getPluginManager().registerEvents(new MainListener(), this);
+
+        getCommand("test").setExecutor(new TestCommand());
     }
 }
--- src/main/java/io/greitan/uniworlds/listeners/MainListener.java
+++ src/main/java/io/greitan/uniworlds/listeners/MainListener.java
@@ -5,9 +5,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.JsonNode;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -15,6 +19,9 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
+
+import de.tr7zw.nbtapi.*;
+import de.tr7zw.nbtapi.iface.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -25,10 +32,11 @@ public class MainListener implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
+
         Player player = event.getPlayer();
         String playerName = player.getName();
-        File playerDataFile = new File(dataFolderPath + "player-" + playerName + ".json");
-    
+        File playerDataFile = new File(dataFolderPath + "player-"+playerName+".json");
+
         if (playerDataFile.exists()) {
             try {
                 ObjectNode playerData = objectMapper.readValue(playerDataFile, ObjectNode.class);
@@ -43,17 +51,72 @@ public class MainListener implements Listener {
                 boolean itemsGiven = playerData.get("itemsGiven").asBoolean(false);
     
                 if (!itemsGiven) {
+                    //Teleport section.
                     double x = playerData.get("data").get("pos").get("x").asDouble();
                     double y = playerData.get("data").get("pos").get("y").asDouble();
                     double z = playerData.get("data").get("pos").get("z").asDouble();
     
                     Location location = new Location(player.getWorld(), x, y, z);
                     player.teleport(location);
-    
-                    // TODO: Выдача
-    
+                    player.sendMessage("U teleported.");
+
+                    //NBT give seсtion.
+                    JsonNode data = playerData.get("data");
+
+                    Bukkit.getLogger().info("Check player inventory.");
+                    if (data.has("inv")) {
+                        for (int slot = 1; slot <= 36; slot++) {
+                            String slotKey = String.valueOf(slot);
+                            
+                            if (data.get("inv").has(slotKey)){
+                                String nbtData = data.get("inv").get(slotKey).asText();
+
+                                ReadWriteNBT nbt = NBT.parseNBT(nbtData);
+                                String str = nbt.toString();
+                                ItemStack itemStack = NBT.itemStackFromNBT(nbt);
+
+                                player.getInventory().setItem(slot - 1, itemStack);
+                            }
+                        }
+
+                        player.updateInventory();
+                        Bukkit.getLogger().info("Inventory give to: " + playerName + "success!");
+                    }
+
+                    if (data.has("echest")) {
+                        for (int slot = 1; slot <= 27; slot++) {
+                            String slotKey = String.valueOf(slot);
+
+                            if (data.get("echest").has(slotKey)) {
+                                String nbtData = data.get("echest").get(slotKey).asText();
+
+                                ReadWriteNBT nbt = NBT.parseNBT(nbtData);
+                                ItemStack itemStack = NBT.itemStackFromNBT(nbt);
+
+                                player.getEnderChest().setItem(slot -1, itemStack);
+                            }
+                        }
+                        player.updateInventory();
+                        Bukkit.getLogger().info("Echest give to: " + playerName + "success!");
+                    }
+
+                    if (data.has("armor")) {
+                        for (int slot = 36; slot <= 39; slot++) {
+                            String slotKey = String.valueOf(slot);
+
+                            if (data.get("armor").has(slotKey)){
+                                String nbtData = data.get("armor").get(slotKey).asText();
+
+                                ReadWriteNBT nbt = NBT.parseNBT(nbtData);
+                                ItemStack itemStack = NBT.itemStackFromNBT(nbt);
+
+                                player.getInventory().setItem(slot, itemStack);
+                            }
+                        }
+                        player.updateInventory();
+                        Bukkit.getLogger().info("Armor give to: " + playerName + " success!");
+                    }
                     playerData.put("itemsGiven", true);
-    
                     ObjectWriter writer = objectMapper.writer(new DefaultPrettyPrinter());
                     writer.writeValue(playerDataFile, playerData);
                 }
@@ -61,8 +124,7 @@ public class MainListener implements Listener {
                 e.printStackTrace();
             }
         } else {
-            TextComponent message = Component.text("U dnt have NBT.").color(NamedTextColor.GREEN);
-            player.sendMessage(message);
+            player.sendMessage("U dnt have NBT.");
         }
     }
 }
